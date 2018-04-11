dfea061f2c311d493d96f63d9249e8a6926dcda7
Marianna <mariannapaciolla@gmail.com>
2018-04-11 17:59:49 +0200

    Aggiunta bacheca activity # Conflicts: #	.idea/vcs.xml

---
+++ app/src/main/java/com/example/marianna/vivinerasmus/BachecaActivity.java
@@ -0,0 +1,17 @@
+package com.example.marianna.vivinerasmus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TabHost;
+
+
+public class BachecaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bacheca);
+    }
+}
